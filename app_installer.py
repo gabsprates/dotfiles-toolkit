e17c4c9723f8b077f25f27414baddf6ae4f24069
Gabriel Prates <gabsprates@gmail.com>
2026-03-02 23:28:54 +0100

    feat(AppInstaller): define interface

--- app_installer.py
+++ app_installer.py
@@ -4,6 +4,28 @@ from pathlib import Path
 
 
 class AppInstaller:
+    """
+    Interface to be implemented for each app that should be installed.
+    """
+
+    def __init__(self, os_id: str):
+        raise NotImplementedError(
+            "Plugins must implement the '__init__' method.")
+
+    def install(self):
+        """
+        Method that executes any sudo actions, as instaling packages.
+        """
+        raise NotImplementedError(
+            "Plugins must implement the 'install' method.")
+
+    def customize(self):
+        """
+        Method that execcutes any user level customization, like editing
+        PATH, or even installing something at user's directory.
+        """
+        raise NotImplementedError(
+            "Plugins must implement the 'customize' method.")
 
     @staticmethod
     def create_symlink(link: Path, target: Path):
